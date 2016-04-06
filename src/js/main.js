381a516025a210f8830714eca28946b333923710
Dominick Peluso <peluso.dominick@gmail.com>
2016-04-06 19:40:11 -0400

    Get typeahead from npm

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
 var logUnderscoreVersion = require('./logVersion');
 var d3 = require("d3");
 var q = require("d3-queue");
+var Bloodhound = require("typeahead.js-browserify").Bloodhound;
 
 var airVis = require('./vis/1_air.js');
 
+// Sanity check
 console.log("src/js/main.js");
-
 logUnderscoreVersion();
 
+// Test libraries are being required properly
 console.log('d3',d3);
 console.log('d3-queue', q);
-
-console.log("hello world");
-
+console.log("bloodhound", Bloodhound);
 
 
 var createVisualization = function(error, worldBankData){
