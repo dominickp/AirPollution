988753b19ba51dbc25ccfbfa2d76a622bde9889f
Dominick Peluso <peluso.dominick@gmail.com>
2016-04-06 19:22:33 -0400

    Add d3-queue
    npm update needed

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 var logUnderscoreVersion = require('./logVersion');
 var d3 = require("d3");
+var q = require("d3-queue");
 
 var airVis = require('./vis/1_air.js');
 
@@ -7,6 +8,17 @@ console.log("src/js/main.js");
 
 logUnderscoreVersion();
 
-console.log(d3);
+console.log('d3',d3);
+console.log('d3-queue', q);
 
 console.log("hello world");
+
+
+
+var createVisualization = function(error, worldBankData){
+    console.log("worldBankData", worldBankData);
+};
+
+q.queue()
+    .defer(d3.csv, "data/World Bank pm2.5 data.xls - Data.csv")
+    .await(createVisualization);
