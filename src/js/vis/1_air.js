55b3cd35e15867ac2dffe9164622007aaa4bb352
Dominick Peluso <peluso.dominick@gmail.com>
2016-04-13 17:33:37 -0400

    Re-order update elements

--- src/js/vis/1_air.js
+++ src/js/vis/1_air.js
@@ -20,28 +20,7 @@ var airVisualization = function(container_selector, service) {
     var width = 400 - margin.left - margin.right,
         height = 150 - margin.top - margin.bottom;
 
-    // Initialize axis and scales
-    model.x = d3.scale.linear()
-        .range([0, width]);
-
-    model.xAxis = d3.svg.axis()
-        .scale(model.x)
-        .orient("bottom");
-
-    // Start SVG
-    model.svg = d3.select(container_selector).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // Start axis
-    model.axis_element = model.svg.append("g")
-        .attr("class", "x-axis axis")
-        .attr("transform", "translate(0," + height + ")");
-
-
-
+    // Helper functions
     model.buildGaugeBackground = function(){
         // Draw the linear gauge body
         var gradient = model.svg.append("defs")
@@ -70,33 +49,63 @@ var airVisualization = function(container_selector, service) {
             .attr("stroke", "grey");
     };
 
-    model.updateVis = function(){
+    // Initialize axis and scales
+    model.x = d3.scale.linear()
+        .range([0, width]);
 
+    model.xAxis = d3.svg.axis()
+        .scale(model.x)
+        .orient("bottom");
+
+    // Start SVG
+    model.svg = d3.select(container_selector).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Start axis
+    model.axis_element = model.svg.append("g")
+        .attr("class", "x-axis axis")
+        .attr("transform", "translate(0," + height + ")");
+
+    model.buildGaugeBackground();
+
+    // Set up these lines in advance
+    model.selected_city = model.svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("height", 60)
+        .attr("width", 2)
+        .attr("fill", "red");
+
+    // WHO Safe Level Line
+    model.safe_level = model.svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("height", 60)
+        .attr("width", 2)
+        .attr("fill", "blue");
+
+
+    model.updateVis = function(){
 
         model.x.domain([
             d3.min(model.data, function(d) {return d[model.selected_unit.key];}),
             d3.max(model.data, function(d) {return d[model.selected_unit.key];})
         ]);
 
-        // Only build the backround guage once
-        if(typeof(model.linear_guage_body) === 'undefined'){
-            model.buildGaugeBackground();
-        }
+        // Update guage body every time
+        model.linear_guage_body
+            .attr("width", model.x(d3.max(model.data, function(d) {return d[model.selected_unit.key];})))
+            .attr("height", 50)
+            .style("fill", "url(#gradient)")
+            .attr("stroke", "grey");
 
 
         model.xAxis.scale(model.x);
         model.svg.select(".x-axis").transition().duration(1500).call(model.xAxis);
 
-        // Only append the safe level line once
-        if(typeof(model.safe_level) === 'undefined'){
-            // WHO Safe Level Line
-            model.safe_level = model.svg.append("rect")
-                .attr("x", model.x(model.selected_unit.safe_level))
-                .attr("y", 0)
-                .attr("height", 60)
-                .attr("width", 2)
-                .attr("fill", "blue");
-        }
 
         // Update safe level line
         model.safe_level
@@ -109,17 +118,6 @@ var airVisualization = function(container_selector, service) {
 
             // Update selected city
             model.active_city_data = service.getSelectedCityData();
-
-            // Only append the selected line once
-            if(typeof(model.selected_city) === 'undefined') {
-
-                model.selected_city = model.svg.append("rect")
-                    .attr("x", model.x(model.active_city_data[model.selected_unit.key]))
-                    .attr("y", 0)
-                    .attr("height", 60)
-                    .attr("width", 2)
-                    .attr("fill", "red");
-            }
 
             // Update safe level line
             model.selected_city
